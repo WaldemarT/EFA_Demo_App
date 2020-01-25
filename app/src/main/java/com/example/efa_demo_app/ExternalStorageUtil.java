bbc8daf3ce55e1cea134f8c190af0094fc93f79a
WaldemarT <waldemar.titov@hs-karlsruhe.de>
2020-01-25 14:16:24 +0100

    formatting

--- app/src/main/java/com/example/efa_demo_app/ExternalStorageUtil.java
+++ app/src/main/java/com/example/efa_demo_app/ExternalStorageUtil.java
@@ -6,44 +6,27 @@ import android.os.StatFs;
 
 import java.io.File;
 
-/**
- * Created by Jerry on 1/22/2018.
- */
-
 public class ExternalStorageUtil {
 
     // Check whether the external storage is mounted or not.
-    public static boolean isExternalStorageMounted() {
+    private static boolean isExternalStorageMounted() {
 
         String dirState = Environment.getExternalStorageState();
-        if(Environment.MEDIA_MOUNTED.equals(dirState))
-        {
-            return true;
-        }else
-        {
-            return false;
-        }
+        return Environment.MEDIA_MOUNTED.equals(dirState);
     }
 
     // Check whether the external storage is read only or not.
     public static boolean isExternalStorageReadOnly() {
 
         String dirState = Environment.getExternalStorageState();
-        if(Environment.MEDIA_MOUNTED_READ_ONLY.equals(dirState))
-        {
-            return true;
-        }else
-        {
-            return false;
-        }
+        return Environment.MEDIA_MOUNTED_READ_ONLY.equals(dirState);
     }
 
 
     // Get private external storage base directory.
-    public static String getPrivateExternalStorageBaseDir(Context context, String dirType)
-    {
+    public static String getPrivateExternalStorageBaseDir(Context context, String dirType) {
         String ret = "";
-        if(isExternalStorageMounted()) {
+        if (isExternalStorageMounted()) {
             File file = context.getExternalFilesDir(dirType);
             ret = file.getAbsolutePath();
         }
@@ -51,10 +34,9 @@ public class ExternalStorageUtil {
     }
 
     // Get private cache external storage base directory.
-    public static String getPrivateCacheExternalStorageBaseDir(Context context)
-    {
+    public static String getPrivateCacheExternalStorageBaseDir(Context context) {
         String ret = "";
-        if(isExternalStorageMounted()) {
+        if (isExternalStorageMounted()) {
             File file = context.getExternalCacheDir();
             ret = file.getAbsolutePath();
         }
@@ -63,10 +45,9 @@ public class ExternalStorageUtil {
 
 
     // Get public external storage base directory.
-    public static String getPublicExternalStorageBaseDir()
-    {
+    private static String getPublicExternalStorageBaseDir() {
         String ret = "";
-        if(isExternalStorageMounted()) {
+        if (isExternalStorageMounted()) {
             File file = Environment.getExternalStorageDirectory();
             ret = file.getAbsolutePath();
         }
@@ -74,10 +55,9 @@ public class ExternalStorageUtil {
     }
 
     // Get public external storage base directory.
-    public static String getPublicExternalStorageBaseDir(String dirType)
-    {
+    static String getPublicExternalStorageBaseDir(String dirType) {
         String ret = "";
-        if(isExternalStorageMounted()) {
+        if (isExternalStorageMounted()) {
             File file = Environment.getExternalStoragePublicDirectory(dirType);
             ret = file.getAbsolutePath();
         }
